bd9e765e19cbd18331f077c3aef57626d3aa1a85
ParshutinD <daniiparshutin1@gmail.com>
2021-05-04 01:47:56 +0300

    Автоматизация сборки личного проекта

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,14 @@ const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
+const htmlmin = require ("gulp-htmlmin");
 const autoprefixer = require("autoprefixer");
 const sync = require("browser-sync").create();
+const rename = require("gulp-rename");
+const terser = require("gulp-terser");
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+const svgstore = require("gulp-svgstore");
 
 // Styles
 
@@ -22,6 +28,89 @@ const styles = () => {
 }
 
 exports.styles = styles;
+
+const stylesmin = () => {
+  return gulp.src("source/less/style.less")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
+}
+
+exports.stylesmin = stylesmin;
+
+// HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+  .pipe(htmlmin({ collapseWhitespace: true }))
+  .pipe(gulp.dest("build"));
+}
+
+exports.html = html;
+
+// Scripts
+
+const scripts = () => {
+  return gulp.src("source/js/script.js")
+  .pipe(terser())
+  .pipe(rename("script.min.js"))
+  .pipe(gulp.dest("build/js"))
+  .pipe(sync.stream());
+}
+
+exports.scripts = scripts;
+
+// Images
+
+const optimazeimages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(imagemin([
+    imagemin.mozjpeg({progressive: true}),
+    imagemin.optipng({optimizationLevel: 3}),
+    imagemin.svgo()
+  ]))
+    .pipe(gulp.dest("build/img"));
+}
+
+exports.images = optimazeimages;
+
+const copyImages = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(gulp.dest("build/img"))
+}
+
+exports.images = copyImages;
+
+
+// WebP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+  .pipe(webp({quality: 90}))
+  .pipe(gulp.dest("build/img"));
+}
+
+exports.createWebp = createWebp;
+
+// Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/*.svg")
+  .pipe(svgstore({
+    inLineSvg: true
+  }))
+  .pipe(rename("sprite.svg"))
+  .pipe (gulp.dest("build/img"));
+}
+
+exports.sprite = sprite;
 
 // Server
 
