3252d2e7e919180803cd25dc4d34cd2b404dd9ca
ParshutinD <daniiparshutin1@gmail.com>
2021-05-09 19:42:44 +0300

    Улучшение скорости загрузки

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,10 @@
 const navMain = document.querySelector(".main-nav");
 const navToggle = document.querySelector(".main-nav__toggle");
+const inputEmail = document.querySelector(".main-form__email ");
+const inputPhone = document.querySelector(".main-form__tel");
+const inputName = document.querySelector(".main-form__name");
+const inputWeight = document.querySelector(".main-form__weight");
+const form = document.querySelector(".main-form__content");
 
 navMain.classList.remove("main-nav--nojs");
 
@@ -13,3 +18,34 @@ navToggle.addEventListener("click", function () {
   }
 });
 
+inputEmail.addEventListener("input", function (event) {
+  if (!inputEmail.validity.valid) {
+    inputEmail.classList.add("error");
+  } else {
+    inputEmail.classList.remove("error");
+  }
+});
+
+inputPhone.addEventListener("input", function (event) {
+  if (!inputPhone.validity.valid) {
+    inputPhone.classList.add("error");
+  } else {
+    inputPhone.classList.remove("error");
+  }
+});
+
+inputName.addEventListener("input", function (event) {
+  if (!inputName.validity.valid) {
+    inputName.classList.add("error");
+  } else {
+    inputName.classList.remove("error");
+  }
+});
+
+inputWeight.addEventListener("input", function (event) {
+  if (!inputWeight.validity.valid) {
+    inputWeight.classList.add("error");
+  } else {
+    inputWeight.classList.remove("error");
+  }
+});
